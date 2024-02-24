52ecb6ef03ce884e5f524b730492ba1d3a45ddfb
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-25 01:53:54 +0300

    Replace goall

--- lib/widgets/description.dart
+++ lib/widgets/description.dart
@@ -1,14 +1,13 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/utils/custom_colors.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Description extends StatelessWidget {
   final bool isVertical;
   final double width;
-  const Description(
-      {required this.isVertical, required this.width, Key? key})
+  const Description({required this.isVertical, required this.width, Key? key})
       : super(key: key);
 
   @override
@@ -26,15 +25,15 @@ class Description extends StatelessWidget {
               alignment: Alignment.center,
               color: CustomColors.primary,
               child: Center(
-                  child: Text('Software Engineer',
+                  child: Text('AI dev',
                       style: GoogleFonts.getFont('Days One',
                           color: Colors.black, fontSize: 10)))),
           SizedBox(height: 0.015 * width),
-          Text('Code is modern poetry',
+          Text('Coding with passion &  glee',
               style: GoogleFonts.getFont('Delius',
                   color: Colors.white, fontSize: 30)),
           const SizedBox(height: 5),
-          Text('Building Africa\'s next Unicorn',
+          Text('Working on the next big thing',
               style: GoogleFonts.getFont('Delius',
                   color: Colors.white, fontSize: 30)),
           const SizedBox(height: 20),
@@ -45,18 +44,16 @@ class Description extends StatelessWidget {
               repeatForever: true,
               pause: const Duration(seconds: 2),
               animatedTexts: [
-                TyperAnimatedText(
-                    "I'm developing mobile,frontend and backend applications",
+                TyperAnimatedText("Am interested in AI and Machine Learning",
                     textAlign: isVertical ? TextAlign.center : TextAlign.start,
                     textStyle: GoogleFonts.getFont('Delius',
-                        color: CustomColors.gray,
-                        fontSize: 15)),
+                        color: CustomColors.gray, fontSize: 15)),
               ],
             ),
           ),
           InkWell(
-            onTap: () async => !await launch(
-                'https://mail.google.com/mail/u/0/?fs=1&to=reecejames934@gmail.com&tf=cm'),
+            onTap: () async => !await launchUrl(Uri.parse(
+                'https://mail.google.com/mail/u/0/?fs=1&to=reecejames934@gmail.com&tf=cm')),
             child: Text("Let's chat",
                 style: GoogleFonts.getFont('Delius',
                     decoration: TextDecoration.underline,
