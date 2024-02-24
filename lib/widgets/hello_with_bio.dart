8ce30911f40111746b9a27a46faad1a877ea7ef1
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-25 01:54:25 +0300

    Remove hardcoded name

--- lib/widgets/hello_with_bio.dart
+++ lib/widgets/hello_with_bio.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/utils/constants.dart';
 import 'package:portfolio/utils/custom_colors.dart';
 
 class HelloWithBio extends StatelessWidget {
   final double width;
   final double ratio;
-  const HelloWithBio({required this.width, required this.ratio,Key? key}) : super(key: key);
+  const HelloWithBio({required this.width, required this.ratio, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +16,11 @@ class HelloWithBio extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Bonjour!  I’m James Njoroge',
+          Text('Bonjour!  I\'m $name',
               style: GoogleFonts.getFont('Delius',
                   color: Colors.white, fontSize: 26)),
           const SizedBox(height: 5),
-          Text(
-              'AI is the new buzz 😁word ! Am researching AI',
+          Text('AI is the new buzz 😁word ! Am interested in Maths and AI',
               style: GoogleFonts.getFont('Delius',
                   color: CustomColors.gray, fontSize: 16)),
         ],
