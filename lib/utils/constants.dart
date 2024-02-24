68b96384441a02a25f619cdbc0e0eacd6a8ed891
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-25 01:49:35 +0300

    Move social links to a separate folder

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+String github = 'https://github.com/kagemanjoroge/';
+String facebook = 'https://www.facebook.com/reece.james.1088/';
+String linkedIn = 'https://www.linkedin.com/in/kagemanjoroge/';
+String twitter = '';
+String email = 'reecejames934@gmail.com';
+String name = 'Kagema Njoroge';
--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/utils/breakpoints.dart';
+import 'package:portfolio/utils/constants.dart';
 import 'package:portfolio/utils/custom_colors.dart';
 
 class Footer extends StatelessWidget {
   final double width;
   final ScrollController scrollController;
-  const Footer({required this.width,required this.scrollController, Key? key}) : super(key: key);
+  const Footer({required this.width, required this.scrollController, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,53 +20,43 @@ class Footer extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              "Kagema Njoroge",
+              name,
               style: GoogleFonts.getFont('Montserrat',
                   color: Colors.greenAccent,
                   fontSize: 40,
                   fontWeight: FontWeight.bold),
             ),
-
-          width > Breakpoints.sm
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                   Text('Copyright © 2023 Kagema Njoroge',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.gray,
-                          fontSize: 14)),
-                  Text('All rights reserved',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.gray,
-                          fontSize: 14)),
-                  Text('reecejames934@gmail.com',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.gray,
-                          fontSize: 14)),
-                ],
-              )
-            : Column(
-                children: [
-                  Text('Copyright © 2023 James Njoroge',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.gray,
-                          fontSize: 14)),
-                          const SizedBox(height: 10),
-                          Text(' All rights reserved',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.gray,
-                          fontSize: 14)),
-                  const SizedBox(height: 10),
-                  Text('reecejames934@gmail.com',
-                      style: GoogleFonts.getFont('Delius',
-                          color: CustomColors.purple,
-                          fontSize: 10)),
-                          
-
-                ],
-              ),
-             
-        ],)
-              );
+            width > Breakpoints.sm
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text('Copyright © ${DateTime.now().year} $name',
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.gray, fontSize: 14)),
+                      Text('All rights reserved',
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.gray, fontSize: 14)),
+                      Text(email,
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.gray, fontSize: 14)),
+                    ],
+                  )
+                : Column(
+                    children: [
+                      Text('Copyright © ${DateTime.now().year} $name',
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.gray, fontSize: 14)),
+                      const SizedBox(height: 10),
+                      Text(' All rights reserved',
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.gray, fontSize: 14)),
+                      const SizedBox(height: 10),
+                      Text(email,
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.purple, fontSize: 10)),
+                    ],
+                  ),
+          ],
+        ));
   }
 }
