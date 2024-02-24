6f46f4b3f98a30f5d25697d3df77e52d87b18693
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-25 01:52:59 +0300

    Replace ASCII char that can cause confusion

--- lib/widgets/info.dart
+++ lib/widgets/info.dart
@@ -5,7 +5,8 @@ import 'package:portfolio/utils/custom_colors.dart';
 class Info extends StatelessWidget {
   final double width;
   final double ratio;
-  const Info({required this.width,required this.ratio ,Key? key}) : super(key: key);
+  const Info({required this.width, required this.ratio, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +15,16 @@ class Info extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-              '• I’m studying Computer Science at the University Of Nairobi.',
+          Text('• studying Computer Science at the University Of Nairobi.',
               style: GoogleFonts.getFont('Delius',
                   color: CustomColors.gray, fontSize: 13)),
           const SizedBox(height: 20),
-          Text('• Am very passionate about code.',
+          Text('• am very passionate about AI and Machine Learning.',
               style: GoogleFonts.getFont('Delius',
                   color: CustomColors.gray, fontSize: 13)),
           const SizedBox(height: 20),
           Text(
-              '• I like tinkering. My goal is simple. It is a complete understanding of technology, why it works and how to make it work for me.',
+              '• I like tinkering. Why does it work? How can I make it better?',
               style: GoogleFonts.getFont('Delius',
                   color: CustomColors.gray, fontSize: 13)),
         ],
